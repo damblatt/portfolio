a673541bdf5ae5decdc0906e799d53bfc27618b2
damblatt <dam.blatt@gmail.com>
2023-01-23 20:27:29 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     colors: {
-      'turquoise': '#2dcfc5',
-      'light-turquoise': '#ABEBE7',
+      'turquoise': '#2DCFC5',
+      'light-turquoise': '#C0F0ED',
+      'dark-turquoise': '#28BAB1',
+      'dark-turquoise-2': '#24A79F',
+      'dark-turquoise-3': '#20948D',
       'white': '#ffffff',
+      'black': '#191919',
     },
     extend: {
       fontFamily: {
