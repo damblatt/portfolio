99ccfffeb47745bce4266024ea8ca978ae25828e
damblatt <dam.blatt@gmail.com>
2023-02-06 13:38:08 +0100

    animated scrolling to top

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,13 @@
-window.onload = function(){
+window.onload = function () {
   const mybutton = document.getElementById('backToTop');
-  console.log(mybutton)
-  mybutton.addEventListener("click", backToTop);
-}
+  console.log(mybutton);
+  mybutton.addEventListener('click', backToTop);
+};
 
 function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
 }
